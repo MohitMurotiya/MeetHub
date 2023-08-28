3fcbd7f6201c6f32bb8cea88ea9be2ae0d72f0a9
MOHIT MUROTIYA <mohitmurotiya98@gmail.com>
2023-08-29 00:37:38 +0530

    create server model
    used react-hook-form & zod for form-validation

---
+++ lib/constants.ts
@@ -0,0 +1,3 @@
+
+export const UNAUTHORIZED = "Unauthorized"
+export const SERVER_IMG_URL = "https://assets-global.website-files.com/6257adef93867e50d84d30e2/636e0a6918e57475a843f59f_icon_clyde_black_RGB.svg";
