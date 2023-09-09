c6b56f11221f1234facb53064fff6e07bcb252ba
MOHIT MUROTIYA <mohitmurotiya98@gmail.com>
2023-09-09 22:45:57 +0530

    generate new invite-code API

---
+++ app/api/servers/[serverId]/invite-code/route.ts
@@ -0,0 +1,37 @@
+import { v4 as uuidv4 } from "uuid";
+import { NextResponse } from "next/server";
+
+import { SERVER_ERROR, UNAUTHORIZED } from "@/lib/constants";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+
+export async function PATCH(
+    req: Request,
+    { params }: { params: { serverId: string } }
+) {
+    try {
+        const profile = await currentProfile();
+        if(!profile) {
+            return new NextResponse(UNAUTHORIZED, { status: 401});
+        }
+
+        if(!params.serverId) {
+            return new NextResponse("Server Id Missing", { status: 404});
+        }
+
+        const server = await db.server.update({
+            where: { 
+                id: params.serverId,
+                profileId: profile.id,
+            },
+            data: {
+                inviteCode: uuidv4()
+            }
+        });
+        return NextResponse.json(server);
+        
+    } catch (err) {
+        console.log("[SERVER_ID]", err);
+        return new NextResponse(SERVER_ERROR, { status: 500});
+    }
+}
