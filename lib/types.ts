4b35a868f69e28b89ea24d1c9046b71f21645c5a
MOHIT MUROTIYA <mohitmurotiya98@gmail.com>
2023-09-18 00:46:54 +0530

    chat-item component added to display a message
    features included:
     - message owner only can edit or delete his message
     - admin or moderator can delete any message

--- lib/types.ts
+++ lib/types.ts
@@ -1,8 +1,15 @@
-import { Member, Profile, Server } from "@prisma/client";
+import { Member, Message, Profile, Server } from "@prisma/client";
 
 export type ServerWithMembersWithProfiles = Server & {
     members: (Member & { profile: Profile })[];
 };
+
+// For response from fetch Message API
+export type MessageWithMemberWithProfile = Message & {
+    member: Member & {
+        profile: Profile
+    }
+}
 
 /**custom response type for socket */
 import { NextApiResponse } from "next";
